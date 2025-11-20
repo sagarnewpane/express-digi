f2f86d7949861e945162712269b0999270a38144
Sagar newpane <sdreamnewpane@gmail.com>
2025-11-20 21:42:46 +0530

    added .env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 let teaData = [
